ab20d09bee8709da68105d7a663501cf081963a3
Paulo G M Santos <31298118+paulogm15@users.noreply.github.com>
2023-01-17 10:13:46 -0300

    :tada: Commit inicial -> setup backend + frontend web

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import Fastify, { fastify } from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get("/", async () => {
+    const habits = await prisma.habit.findMany()
+
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log("HTTP Server running on port 3333!")
+})
